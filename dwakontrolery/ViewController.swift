0c23fb3f18f8060dc13ab77ae53288b6b9aa12e1
Patryk Cyrek <patryk.cyrek@yahoo.com>
2019-10-28 16:28:37 +0100

    Initial Commit

---
+++ dwakontrolery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dwakontrolery
+//
+//  Created by Patryk Cyrek on 28/10/2019.
+//  Copyright © 2019 Patryk Cyrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
